9f2225539fd7c1a75463872ec3604ba9fdf87d69
Ethan <ethansagin@gmail.com>
2019-03-13 20:27:28 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,6 @@
 def reverse_each_word(sentence)
   sent_arr = sentence.split
-  sent_arr.collect do {|word| word.reverse!}
-  end
+  sent_arr.collect {|word| word.reverse!}
   sent_arr.join(" ")
 end
 
