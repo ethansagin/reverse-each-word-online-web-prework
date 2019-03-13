51a6f20d10e08818630e77e2a5db8759bbd61dec
Ethan <ethansagin@gmail.com>
2019-03-13 20:27:39 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -5,8 +5,3 @@ def reverse_each_word(sentence)
 end
 
 
-#take argument of sentence
-#string to array at " "
-#iterate over each element and reverse strings
-#return array of reversed strings
-#array to string
