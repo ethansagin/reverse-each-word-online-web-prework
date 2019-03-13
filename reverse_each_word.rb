fcd08f62d5f92bb17c4889d1ec0ab2e56579d3d8
Ethan <ethansagin@gmail.com>
2019-03-13 20:15:00 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,10 @@
 def reverse_each_word(sentence)
   sent_arr = sentence.split('')
-  sent_arr.each do |word|
-    word.reverse!
-  end
-  sent_arr
 end
+
+
+#take argument of sentence
+#string to array at " "
+#iterate over each element and reverse strings
+#return array of reversed strings
+#array to string
